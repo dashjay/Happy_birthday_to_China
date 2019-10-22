cac97606a86f4cd3088b25b91bc9d958dde7c860
dashjay <dashjay@foxmail.com>
2019-10-22 08:45:19 +0800

    upload by script

--- app.py
+++ app.py
@@ -62,9 +62,10 @@ def position():
         })
     ip = request.remote_addr
 
-    formatstirng = "ip:{0},pos:{1},addr:{2}".format(ip,lng_lat,addr)
+    formatstirng = "pos:{1},addr:{2}".format(lng_lat,addr)
     import os
-    os.system("echo " + formatstirng + ">> test.txt")
+    sql = "INSERT INTO position (ip,pos) VALUES ('{0}','{1}')".format(ip,formatstirng)
+    run_sql(sql)
     import cpca
     df = cpca.transform([addr])
     province = ''
